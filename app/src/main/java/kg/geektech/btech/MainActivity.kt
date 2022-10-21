2fb6c61fee087892cd2ee0074c35964ac8498abc
bek2rkanatbekov <Bekalone>
2022-10-21 20:16:04 +0600

    Initial commit

---
+++ app/src/main/java/kg/geektech/btech/MainActivity.kt
@@ -0,0 +1,12 @@
+package kg.geektech.btech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kg.geektech.btech.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
