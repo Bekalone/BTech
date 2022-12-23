3c61b564fe7b0551294f11db5f9b4183e149eb94
bek2rkanatbekov <Bekalone>
2022-12-23 15:11:44 +0600

    provide api и dao исправил

--- app/src/main/java/kg/geektech/btech/data/repository/GadgetRepositoryImp.kt
+++ app/src/main/java/kg/geektech/btech/data/repository/GadgetRepositoryImp.kt
@@ -4,16 +4,25 @@ import com.example.seventhmfirsthm.data.local.GadgetDao
 import kg.geektech.btech.data.network.api.GadgetApi
 import kg.geektech.btech.data.network.model.GadgetDTO
 import kg.geektech.btech.domain.repository.GadgetRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GadgetRepositoryImp @Inject constructor(private val api: GadgetApi) : GadgetRepository {
-
-    override suspend fun getDetailProduct(): ArrayList<GadgetDTO> {
+class GadgetRepositoryImp @Inject constructor(private val api: GadgetApi,private val gadgetDao: GadgetDao) : GadgetRepository {
+    override suspend fun getDetailProduct(): Flow<List<GadgetDTO>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getProducts(id: Int): GadgetDTO {
+    override suspend fun getProducts(id: Int): Flow<List<GadgetDTO>> {
         TODO("Not yet implemented")
     }
+
+    override suspend fun addGadgetBasket(gadgetDTO: GadgetDTO): Flow<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteGadgetBasket(gadgetDTO: GadgetDTO): Flow<Unit> {
+        TODO("Not yet implemented")
+    }
+
 
 }
