e79b546895c306b8b04cf5bec4c89a6f88bbcf78
bek2rkanatbekov <Bekalone>
2022-12-23 15:43:00 +0600

    переделал модельки domain и data GADGET

--- app/src/main/java/kg/geektech/btech/domain/model/Gadget.kt
+++ app/src/main/java/kg/geektech/btech/domain/model/Gadget.kt
@@ -1,6 +1,7 @@
 package kg.geektech.btech.domain.model
 
 data class Gadget(
+    val id: Int = DEFAULT_ID,
     val image: String? = null, // http://164.92.190.147:8006/media/media/%D0%A7%D1%82%D0%BE-%D1%82%D0%B0%D0%BA%D0%BE%D0%B5-%D1%84%D0%BE%D1%82%D0%BE.jpeg
     val price: Int? = null, // 45000
     val subtext: String? = null, // рассрочка от 489с./ месяц
